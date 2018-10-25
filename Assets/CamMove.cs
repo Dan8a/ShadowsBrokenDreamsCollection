199d1b497fd482fb0de8f40a7f89dc6dac91a9db
Dan8a <ochoad@usc.edu>
2018-10-24 21:20:57 -0700

    Initial commit

---
+++ Assets/CamMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMove : MonoBehaviour {
+
+    private Vector2 velocity;
+
+    public float smoothTimeY;
+    public floats
+
+	void Start () {
+		
+	}
+	
+}
